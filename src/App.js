07941439ff419c638b93564162c2332c06056116
LAPTOP-L74LB9B0\TM <tamanash1997@gmail.com>
2023-06-25 17:11:27 +0530

    Comment Changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Tooltip from './Tooltip';
 
-// Main App Element to be 
+// Main App Element to be rendered on page
 function App() {
   return (
     <div className="sample-text">
       Hello you can - &nbsp;
+        {/* Tooltip Component with Position as Prop. Positions can be Top, Left, Bottom, Right */}
         <Tooltip position="left">
+          {/* Hover Element being sent as Child to Tooltip */}
           <span><u>Hover Over Me !</u></span>
         </Tooltip>
     </div>
--- src/Tooltip.js
+++ src/Tooltip.js
@@ -3,7 +3,9 @@ import './Tooltip.css'
 const Tooltip = (props) => {
     return (
         <div className='tooltip-container'>
+            {/* Element to hover over to show tooltip */}
             {props.children}
+            {/* Postion prop for rendering tooltip in given position */}
             <div className={`tooltip tooltip-${props.position}`}>
                 <div className={`tooltip-arrow arrow-${props.position}`}></div>
                 <span>Thanks for Hovering! I'm a Tooltip</span>
